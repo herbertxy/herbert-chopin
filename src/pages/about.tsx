c18482a5ba8bcdcfa36bf72a29a0f001dc5377f9
herbert <408284566@qq.com>
2022-10-29 13:50:34 +0800

    test update

---
+++ src/pages/about.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+const About: React.FC = () => {
+  return (
+    <main>
+      <h2>about me:</h2>
+      <div>name: herbert chopin</div>
+      <div>i love gym, bike and music</div>
+    </main>
+  );
+};
+
+export default About
